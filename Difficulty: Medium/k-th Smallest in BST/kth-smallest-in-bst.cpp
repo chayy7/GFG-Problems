6ed7f04f6034023d4303ac67b8b94cb83041ea53
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2025-11-07 05:10:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k-th Smallest in BST/kth-smallest-in-bst.cpp
@@ -0,0 +1,36 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void inorder(Node* root,vector<int>& v){
+        if(root == nullptr) return;
+        
+        inorder(root->left, v);
+        v.push_back(root->data);
+        inorder(root->right, v);
+    }
+    int kthSmallest(Node *root, int k) {
+        // code here
+        vector<int> v;
+        inorder(root,v);
+        
+        if(v.size() < k){
+            return -1;
+        }
+        
+        return v[k-1];
+        
+    }
+};
