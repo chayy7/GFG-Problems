b5190c5717ac498194ed7ee52050aba35a208d73
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-02-21 22:38:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -1,39 +1,40 @@
 class Solution {
   public:
-    // Function to find common elements in three arrays.
-    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,
+    vector<int> commonElements(vector<int> &arr1,
+                               vector<int> &arr2,
                                vector<int> &arr3) {
-        // Code Here
-        set<int> s1(arr1.begin(),arr1.end());
-        set<int> s2(arr2.begin(),arr2.end());
-        set<int> s3(arr3.begin(),arr3.end());
+
         vector<int> v;
-        for(auto i: s1){
-            v.push_back(i);
-        }
-        for(auto i: s2){
-            v.push_back(i);
-        }
-        for(auto i: s3){
-            v.push_back(i);
-        }
-        
-        map<int,int> mpp;
-        for(int i = 0;i<v.size();i++){
-            mpp[v[i]]++;
-        }
-        int n= v.size();
-        vector<int> c;
-        for(auto i: mpp){
-            
-            if(i.second > 2){
-                c.push_back(i.first);
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.size() && j < arr2.size() && k < arr3.size()) {
+
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+                v.push_back(arr1[i]);
+
+                int val = arr1[i];
+
+                while (i < arr1.size() && arr1[i] == val) i++;
+                while (j < arr2.size() && arr2[j] == val) j++;
+                while (k < arr3.size() && arr3[k] == val) k++;
+            }
+            else {
+                int mn = min({arr1[i], arr2[j], arr3[k]});
+
+                if (arr1[i] == mn) {
+                    int val = arr1[i];
+                    while (i < arr1.size() && arr1[i] == val) i++;
+                }
+                else if (arr2[j] == mn) {
+                    int val = arr2[j];
+                    while (j < arr2.size() && arr2[j] == val) j++;
+                }
+                else {
+                    int val = arr3[k];
+                    while (k < arr3.size() && arr3[k] == val) k++;
+                }
             }
         }
-        if(c.size() == 0){
-            return {-1};
-        }
-        
-        return c;
+        return v;
     }
 };
