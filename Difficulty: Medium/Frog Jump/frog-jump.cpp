519e083f6368a19cda32acc15c6b1739c7d4ba69
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-11 11:48:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,21 +1,27 @@
 class Solution {
   public:
-    int f(int i, vector<int>& h, vector<int>& dp) {
-        if (i == 0) return 0;
-        if (dp[i] != -1) return dp[i];
-    
-        int one = f(i-1, h, dp) + abs(h[i] - h[i-1]);
+    int func(int n, vector<int>& height,vector<int>& dp){
+        if(n == 0){
+            
+            dp[n] = 0;
+            return 0;
+        }
+        
+        if(dp[n] != -1) return dp[n];
+        int one = abs(height[n]-height[n-1]) + func(n-1,height, dp);
+        
         int two = INT_MAX;
-    
-        if (i > 1)
-            two = f(i-2, h, dp) + abs(h[i] - h[i-2]);
-    
-        return dp[i] = min(one, two);
+        if(n>1) two = abs(height[n]-height[n-2]) + func(n-2,height, dp);
+        
+        return dp[n] = min(one,two);
     }
     int minCost(vector<int>& height) {
         // Code here
-        int n=height.size();
-        vector<int> dp(n,-1);
-        return f(n-1, height, dp);
+        int n= height.size();
+        int sum = 0;
+        vector<int> dp(n+1, -1);
+        return func(n-1, height, dp);
+        
+        
     }
 };
