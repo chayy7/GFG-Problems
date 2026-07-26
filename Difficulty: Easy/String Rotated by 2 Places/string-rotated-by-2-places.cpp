093923d0451b2fa9544ef8b086f247ff628b66ec
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-07-26 17:48:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.cpp
@@ -1,35 +1,26 @@
 class Solution {
   public:
-    void lror(string& lr){
-        reverse(lr.begin(),lr.end()-1);
-        reverse(lr.end()-1,lr.end());
-        reverse(lr.begin(),lr.end());
+    string lr(string s1){
+        reverse(s1.begin(), s1.begin()+2);
+        reverse(s1.begin()+2, s1.end());
+        reverse(s1.begin(), s1.end());
+        
+        return s1;
     }
-    void rror(string& rr){
-        reverse(rr.begin(),rr.begin()+1);
-        reverse(rr.begin()+1,rr.end());
-        reverse(rr.begin(),rr.end());
+    string rr(string s1){
+        reverse(s1.begin(), s1.end()-2);
+        reverse(s1.end()-2, s1.end());
+        reverse(s1.begin(), s1.end());
+        
+        return s1;
     }
     bool isRotated(string& s1, string& s2) {
         // code here
-        string lr = s1;
-        string rr = s1;
+        if(s1.size() <= 1 || s2.size() <= 1) return false;
         
-        lror(lr);
-        lror(lr);
-        // cout << lr << " ";
-        if(lr == s2){
-            return 1;
-        }
-        rror(rr);
-        rror(rr);
-        // cout << rr << " ";
-        if(rr == s2){
-            return 1;
-        }
+        string a = lr(s1);
+        string b = rr(s1);
         
-        return 0;
-        
-        
+        return a == s2 || b == s2;
     }
 };
