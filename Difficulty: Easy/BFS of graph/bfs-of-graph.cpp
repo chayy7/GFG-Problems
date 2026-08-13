3a21d8580cf10155b64d90e5ada6be8b71264d77
Chaitanya Reddy <chaitanyasrinu4735@gmail.com>
2026-08-13 22:55:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -2,26 +2,55 @@ class Solution {
   public:
     vector<int> bfs(vector<vector<int>> &adj) {
         // code here
+        map<int, vector<int>> mpp;
+        for(int i=0;i<adj.size();i++){
+            for(int j=0;j<adj[i].size();j++){
+                mpp[i].push_back(adj[i][j]);
+                // mpp[adj[i][j]].push_back(i);
+            }
+        }
+        
+        // for(auto i:mpp){
+        //     cout << i.first << "->";
+        //     for(auto j:i.second){
+        //         cout << j << " ";
+                
+        //     }
+        //     cout << endl;
+        // }
+        
+        int start = 0;
+        
+        map<int, bool> visited;
+        visited[start] = true;
+        
+        
+        queue<int> q;
+        q.push(start);
+        
+        
         
         vector<int> ans;
-        int n=adj.size();
         
-        vector<int> vis(n, false);
-        queue<int> q;
-        q.push(0);
-        vis[0] = true;
         
         while(!q.empty()){
-            int front = q.front(); q.pop();
-            ans.push_back(front);
-            for(auto i:adj[front]){
-                if(!vis[i]){
-                    vis[i] = true;
+            auto node = q.front();
+            q.pop();
+            
+            
+            ans.push_back(node);
+            
+            for(auto i:mpp[node]){
+                if(!visited[i]){
+                    visited[i] = true;
                     q.push(i);
                 }
             }
         }
         
+        
+        
         return ans;
+        
     }
 };
